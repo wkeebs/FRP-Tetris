626edabf79da6912839bf004ad1cabae6403e483
Will Keeble <will.a.keeble@gmail.com>
2023-08-18 14:30:45 +1000

    Handled collision with the left and right side of the board

--- src/state.ts
+++ src/state.ts
@@ -42,18 +42,37 @@ const initialState: State = {
 } as const;
 
 //////////////// ACTION CLASSES //////////////////////
+/**
+ * 
+ */
 class Move implements Action {
   constructor(public readonly x: number, public readonly y: number) {}
+  /**
+   * Computes a new state based on the movement of the current piece.
+   * @param s The old state.
+   * @returns The new state.
+   */
   apply = (s: State): State => {
+    // True if any of the piece is at the rightmost limit of the board.
+    const atRight = s.piece.some(
+      (c: Cube) => c.x + this.x > Viewport.CANVAS_WIDTH - Constants.MOVE_BY
+    );
+    // True if any of the piece is at the leftmost limit of the board.
+    const atLeft = s.piece.some(
+      (c: Cube) => c.x + this.x < 0
+    );
     return handleCollisions({
       ...s,
-      piece: s.piece.map((cube: Cube) => {
-        return {
-          ...cube,
-          x: cube.x + this.x,
-          y: cube.y + this.y,
-        };
-      }),
+      piece:
+        atRight || atLeft
+          ? s.piece :
+          s.piece.map((cube: Cube) => {
+              return {
+                ...cube,
+                x: cube.x + this.x,
+                y: cube.y + this.y,
+              };
+            }),
     });
   };
 }
@@ -77,26 +96,19 @@ class Tick implements Action {
 }
 
 //////////////// STATE UPDATES //////////////////////
-// const moveCube =
-//   (move: Move) =>
-//   (cube: Cube): Cube => {
-//     return {
-//       ...cube,
-//       x: cube.x + move.x,
-//       y: cube.y + move.y,
-//     };
-//   };
 
 const handleCollisions = (s: State): State => {
+  // Is a cube at the bottom?
   const hitBottom = (c: Cube) =>
-    c.y >= Viewport.CANVAS_HEIGHT - Constants.MOVE_BY;
-
+  c.y >= Viewport.CANVAS_HEIGHT - Constants.MOVE_BY;
+  
+  // True if the any part of piece is at the bottom of the board.
   const pieceAtBottom = s.piece.some(hitBottom);
 
   return {
     ...s,
     piece: pieceAtBottom ? [] : s.piece,
-    cubes: pieceAtBottom ? s.cubes.concat(s.piece) : s.cubes
+    cubes: pieceAtBottom ? s.cubes.concat(s.piece) : s.cubes,
   };
 };
 
